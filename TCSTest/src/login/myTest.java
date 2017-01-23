b3c87795dae5074edf9ddabbc3cc3844705f7a03
Raj Shashi <raj8aus@gmail.com>
2017-01-23 11:25:06 +1100

    Update myTest.java

--- TCSTest/src/login/myTest.java
+++ TCSTest/src/login/myTest.java
@@ -13,6 +13,8 @@ public class myTest {
 		BufferedReader br = new BufferedReader(fr);
 
 		//Read a Text file containing browser names and set the drivers accordingly and do the actions.
+		//setDriver.set - This sets the driver according to browser name and returns the driver.
+		//input.Data - This basically does the navigation and inputs all the data.
 		for (int i = 0; i <= 2; i++) {
 			input.Data(setDriver.set(br.readLine().trim()));
 		}
